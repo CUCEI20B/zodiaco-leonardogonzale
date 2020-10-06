d4f8912e1d1b5be544397dd552b51d9b7e222e7c
Leonardo González <leonardo.gonzalez3077@alumnos.udg.mx>
2020-10-05 22:36:08 -0500

    Solucion de errores

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-input('Que caballero de oro del zodiaco eres?') 
+input() 
 
 mes = int(input('Ingrese su mes de nacimiento:' ))
 dia = int(input('Ingrese su día de nacimiento: '))
 
 if (dia>=21 and mes==3) or (dia<=20 and mes==4):
-    print ('Eres el caballero de Aries')
+    print ('aries')
 if (dia>=24 and mes==9) or (dia<=23 and mes==10):
-    print ('Eres el caballero de Libra')
+    print (' libra')
 if (dia>=21 and mes==4) or (dia<=21 and mes==5):
-    print ('Eres el caballero de Tauro')
+    print (' tauro')
 if (dia>=24 and mes==10) or (dia<=22 and mes==11):
-    print ('Eres el caballero de Escorpio')
+    print ('escorpio')
 if (dia>=22 and mes==5) or (dia<=21 and mes==6):
-    print ('Eres el caballero de Geminis')
+    print (' geminis')
 if (dia>=23 and mes==11) or (dia<=21 and mes==12):
-    print ('Eres el caballero de Sagitario')
+    print (' sagitario')
 if (dia>=21 and mes==6) or (dia<=23 and mes==7):
-    print ('Eres el caballero de Cancer')
+    print ('cancer')
 if (dia>=22 and mes==12) or (dia<=20 and mes==1):
-    print ('Eres el caballero de Capricornio')
+    print ('capricornio')
 if (dia>=24 and mes==7) or (dia<=23 and mes==8):
-    print ('Eres el caballero de Leo')
+    print ('leo')
 if (dia>=21 and mes==1) or (dia<=19 and mes==2):
-    print ('Eres el caballero de Acuario')
+    print ('acuario')
 if (dia>=24 and mes==8) or (dia<=23 and mes==9):
-    print ('Eres el caballero de Virgo')
+    print ('virgo')
 if (dia>=20 and mes==2) or (dia<=20 and mes==3):
-    print ('Eres el caballero de Piscis')
+    print ('piscis')
