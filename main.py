0d43018419dcb807a28891b3060309b1ea8dbc26
Michel Dávalos <michel.davalos@academicos.udg.mx>
2020-10-07 14:06:51 +0000

    correcciones:

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-input() 
+dia = int(input())
+mes = int(input())
 
-mes = int(input('Ingrese su mes de nacimiento:' ))
-dia = int(input('Ingrese su día de nacimiento: '))
 
 if (dia>=21 and mes==3) or (dia<=20 and mes==4):
     print ('aries')
 if (dia>=24 and mes==9) or (dia<=23 and mes==10):
-    print (' libra')
+    print ('libra')
 if (dia>=21 and mes==4) or (dia<=21 and mes==5):
-    print (' tauro')
+    print ('tauro')
 if (dia>=24 and mes==10) or (dia<=22 and mes==11):
     print ('escorpio')
 if (dia>=22 and mes==5) or (dia<=21 and mes==6):
-    print (' geminis')
+    print ('geminis')
 if (dia>=23 and mes==11) or (dia<=21 and mes==12):
-    print (' sagitario')
+    print ('sagitario')
 if (dia>=21 and mes==6) or (dia<=23 and mes==7):
     print ('cancer')
 if (dia>=22 and mes==12) or (dia<=20 and mes==1):
